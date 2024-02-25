efb8517bc5c8906713bfae228d7449defdf41342
José Carlos de Camargo <jccamargo15@gmail.com>
2024-02-25 14:09:14 -0300

    feat: Start screen

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,39 @@ document.querySelector('#dark-mode-toggle').addEventListener('click', () => {
   document.querySelector('meta[name="theme-color"').setAttribute('content', isDarkMode ? '#1a1a2e' : '#fff');
 });
 
+// initial value 
+
+const name_input = document.querySelector('#input-name');
+const start_screen = document.querySelector('#start-screen');
+
+let level_index = 0;
+let level = CONSTANT.LEVEL[level_index];
+
+// -------
+
+// add button event
+document.querySelector('#btn-level').addEventListener('click', (e) => {
+  level_index = level_index + 1 > CONSTANT.LEVEL.length - 1 ? 0 : level_index + 1;
+  level = CONSTANT.LEVEL[level_index];
+  e.target.innerHTML = CONSTANT.LEVEL_NAME[level_index];
+});
+
+document.querySelector('#btn-play').addEventListener('click', () => {
+  if (name_input.value.trim().length > 0) {
+      // initSudoku();
+      // startGame();
+      alert(`level => ${level}`);
+  } else {
+      name_input.classList.add('input-err');
+      setTimeout(() => {
+          name_input.classList.remove('input-err');
+          name_input.focus();
+      }, 500);
+  }
+});
+
+const getGameInfo = () => JSON.parse(localStorage.getItem('game'));
+
 const init = () => {
   const darkmode = JSON.parse(localStorage.getItem('darkmode'));
   document.body.classList.add(darkmode ? 'dark' : 'light');
